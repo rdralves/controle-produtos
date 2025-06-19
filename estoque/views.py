e7dc0c58c735fa4664bb436d070e0ce5114fa009
rdralves <rdr.alves@gmail.com>
2025-06-19 16:50:20 -0300

    Implementação completa do CRUD para produtos

--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,6 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Produto
@@ -7,3 +10,34 @@ from .models import Produto
 def produto_list(request):
     produtos = Produto.objects.all()
     return render(request, 'estoque/produto_list.html', {'produtos': produtos})
+
+
+class ProdutoListView(LoginRequiredMixin, ListView):
+    model = Produto
+    template_name = 'estoque/produto_list.html'
+    context_object_name = 'produtos'
+
+
+class ProdutoDetailView(LoginRequiredMixin, DetailView):
+    model = Produto
+    template_name = 'estoque/produto_detail.html'
+
+
+class ProdutoCreateView(LoginRequiredMixin, CreateView):
+    model = Produto
+    fields = ['ref_produto', 'descricao', 'quantidade', 'valor']
+    template_name = 'estoque/produto_form.html'
+    success_url = reverse_lazy('produto_list')
+
+
+class ProdutoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Produto
+    fields = ['ref_produto', 'descricao', 'quantidade', 'valor']
+    template_name = 'estoque/produto_form.html'
+    success_url = reverse_lazy('produto_list')
+
+
+class ProdutoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Produto
+    template_name = 'estoque/produto_confirm_delete.html'
+    success_url = reverse_lazy('produto_list')
