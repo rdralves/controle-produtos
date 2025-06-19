e3d696085630a061a77613851426d27b851f3aaf
rdralves <rdr.alves@gmail.com>
2025-06-19 15:29:43 -0300

    Adição do admin

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Produto
 # Register your models here.
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('ref_produto', 'descricao',
+                    'quantidade', 'valor', 'data_entrada')
+    search_fields = ('ref_produto', 'descricao')
