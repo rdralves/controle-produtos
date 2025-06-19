9ae70a9fa337f120c83e2c0463635538ba823411
rdralves <rdr.alves@gmail.com>
2025-06-19 15:39:51 -0300

    add model Movimentacao

--- estoque/models.py
+++ estoque/models.py
@@ -10,3 +10,20 @@ class Produto(models.Model):
 
     def __str__(self):
         return f"{self.ref_produto} - {self.descricao}"
+
+
+class Movimentacao(models.Model):
+    TIPO_MOVIMENTO = (
+        ('E', 'Entrada'),
+        ('S', 'Saída'),
+    )
+
+    produto = models.ForeignKey(
+        Produto, on_delete=models.CASCADE, related_name='movimentacoes')
+    tipo = models.CharField(max_length=1, choices=TIPO_MOVIMENTO)
+    quantidade = models.PositiveIntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+    observacao = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} - {self.produto.ref_produto} ({self.quantidade})"
