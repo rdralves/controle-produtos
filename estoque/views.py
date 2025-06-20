e8e253836d64628d22294161c73caca3348e3429
rdralves <rdr.alves@gmail.com>
2025-06-20 09:47:59 -0300

    Implementação de relatórios e exportação para Excel

--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,8 @@
+from openpyxl.utils import get_column_letter
+import openpyxl
+from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -41,3 +46,45 @@ class ProdutoDeleteView(LoginRequiredMixin, DeleteView):
     model = Produto
     template_name = 'estoque/produto_confirm_delete.html'
     success_url = reverse_lazy('produto_list')
+
+
+# views.py
+
+
+class RelatorioProdutosView(LoginRequiredMixin, ListView):
+    model = Produto
+    template_name = 'estoque/relatorio_produtos.html'
+    context_object_name = 'produtos'
+
+
+class ExportarProdutosExcelView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        produtos = Produto.objects.all()
+
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+        worksheet.title = 'Produtos'
+
+        headers = ['Referência', 'Descrição',
+                   'Quantidade', 'Valor', 'Data Entrada']
+        worksheet.append(headers)
+
+        for produto in produtos:
+            worksheet.append([
+                produto.ref_produto,
+                produto.descricao,
+                produto.quantidade,
+                float(produto.valor),
+                produto.data_entrada.strftime('%d/%m/%Y')
+            ])
+
+        for col_num, column_title in enumerate(headers, 1):
+            column_letter = get_column_letter(col_num)
+            worksheet.column_dimensions[column_letter].width = 20
+
+        response = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = 'attachment; filename=produtos.xlsx'
+        workbook.save(response)
+        return response
